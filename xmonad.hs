acf03d7d1b1a628053663f6bfbc8ac3848d56f9a
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-08-02 15:27:38 +0100

    Added dock toggle

--- xmonad.hs
+++ xmonad.hs
@@ -3,6 +3,7 @@ import XMonad.Util.EZConfig
 import XMonad.Operations
 import XMonad.Util.Dzen
 import XMonad.Hooks.DynamicLog (dynamicLog, dzen)
+import XMonad.Hooks.ManageDocks
 import XMonad.Layout.ResizableTile
 import System.IO
 import XMonad.Actions.Volume
@@ -12,15 +13,23 @@ import Data.Monoid (mappend)
 -- Volume control stuff from: http://dmwit.com/volume/
 alert = dzenConfig centred . show . round
 
+terminus = "-*-terminus-*-*-*-*-12-*-*-*-*-*-*-*"
+bigHelvetica = "-*-helvetica-*-r-*-*-64-*-*-*-*-*-*-*"
+
 centred = onCurr (center 150 66)
-    >=> font "-*-helvetica-*-r-*-*-64-*-*-*-*-*-*-*"
+    >=> font bigHelvetica
     >=> addArgs ["-fg", "#80c0ff"]
     >=> addArgs ["-bg", "#000040"]
 
 -- rebind Mod to the windows key
 modm = mod4Mask
 
-myLayout = ResizableTall 1 (4/100) (3/5) [] ||| Full 
+myLayout = avoidStruts $ tiled ||| Mirror tiled ||| Full
+    where
+        tiled = ResizableTall nmaster increment ratio []
+        nmaster = 1
+        increment = 4/100
+        ratio = 3/5
 
 -- MOD + SHIFT + L => lock screen
 -- f11 => lower volume
@@ -30,7 +39,8 @@ myKeys = [
     ((0, xK_F11), lowerVolume 4 >>= alert),
     ((0, xK_F12), raiseVolume 4 >>= alert),
     ((modm, xK_u), sendMessage MirrorShrink),
-    ((modm, xK_i), sendMessage MirrorExpand)
+    ((modm, xK_i), sendMessage MirrorExpand),
+    ((modm, xK_b), sendMessage ToggleStruts)
     ]
 
 main = do
